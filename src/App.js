b8aa1bcbdb633aafcda7400acefafd839613875d
Logan Sorensen <loganmsorensen@gmail.com>
2020-01-13 13:28:34 -0800

    built out components and added the ability to add a new item to the list

--- src/App.js
+++ src/App.js
@@ -1,6 +1,55 @@
 import React from 'react';
+import TodoList from './components/TodoComponents/TodoList';
+import TodoForm from './components/TodoComponents/TodoForm';
+import './components/TodoComponents/Todo.css';
+
+const todo = [
+  {
+    task: 'Organize Garage',
+    id: 1528817077286,
+    completed: false
+  },
+  {
+    task: 'Bake Cookies',
+    id: 1528817084358,
+    completed: false
+  }
+]
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todoList: todo
+    }
+  }
+
+  toggleTodo = id => {
+    const newTodoList = this.state.todoList.map(todo => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          completed: !todo.completed
+        };
+      } else {
+        return todo;
+      }
+    });
+    this.setState({
+      todoList: newTodoList
+    });
+  };
+
+  addTodo = todoTask => {
+    const newTodo = {
+      task: todoTask,
+      id: Date.now(),
+      completed: false
+    };
+    this.setState({
+      todoList: [...this.state.todoList, newTodo]
+    });
+  };
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
@@ -8,6 +57,11 @@ class App extends React.Component {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        <TodoForm addTodo={this.addTodo} />
+        <TodoList
+        todo={this.state.todoList}
+        toggleTodo={this.toggleTodo}
+        />
       </div>
     );
   }
